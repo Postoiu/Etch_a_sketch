4be644f2a3cdc2f4a96f647e6fb6821e97537fb7
Ovidiu Postoiu <postoiu.o@gmail.com>
2021-02-18 20:54:58 +0200

    Added the posibility to change colors and the Random button generates random colors everytime the mouse hovers over a div

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ let squareDim = 500 / dim - 2;
 const container = document.getElementById('container');
 const newBoard = document.getElementById('newBoard');
 const clear = document.getElementById('clear');
+const red = document.getElementById('red');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+const random = document.getElementById('random');
 
 createGrid(dim);
 
@@ -28,14 +32,43 @@ function createGrid(dimension) {
   const divs = document.querySelectorAll('div.square');
   divs.forEach((div) => {
     div.setAttribute('style', `width: ${squareDim}px; height: ${squareDim}px`);
-    div.addEventListener('mouseover', function(e) {
-        div.setAttribute('style', `width: ${squareDim}px; height: ${squareDim}px; 
-        background-color: red`);
-    });
+    
     clear.addEventListener('click', (e) => {
       div.setAttribute('style', `width: ${squareDim}px; height: ${squareDim}px; 
         background-color: white`);
     })
+
+    red.addEventListener('click', (e) => {
+      div.addEventListener('mouseover', function(e) {
+        div.setAttribute('style', `width: ${squareDim}px; height: ${squareDim}px; 
+        background-color: red`);
+      });
+    });
+
+    blue.addEventListener('click', (e) => {
+      div.addEventListener('mouseover', function(e) {
+        div.setAttribute('style', `width: ${squareDim}px; height: ${squareDim}px; 
+        background-color: blue`);
+      });
+    });
+
+    green.addEventListener('click', (e) => {
+      div.addEventListener('mouseover', function(e) {
+        div.setAttribute('style', `width: ${squareDim}px; height: ${squareDim}px; 
+        background-color: green`);
+      });
+    });
+
+    random.addEventListener('click', (e) => {
+      div.addEventListener('mouseover', function(e) {
+        div.setAttribute('style', `width: ${squareDim}px; height: ${squareDim}px; 
+        background-color: ${setBg()}`);
+      });
+    });
   });
 }
 
+const setBg = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return '#'+randomColor;
+}
